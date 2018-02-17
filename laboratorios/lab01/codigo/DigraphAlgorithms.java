78d64a5ad24d866df53db874283d7da82240fbda
rvillegasm <rvillegasm@eafit.edu.co>
2018-02-16 19:12:00 -0500

    taller 3 y parte del lab 1

---
+++ laboratorios/lab01/codigo/DigraphAlgorithms.java
@@ -0,0 +1,168 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.HashMap;
+/**
+ * This class contains algorithms for digraphs
+ * Adapted from: http://cs.fit.edu/~ryan/java/programs/graph/Dijkstra-java.html
+ * @author Mauricio Toro
+ * @version 1
+ */
+public class DigraphAlgorithms
+{
+
+  public static int maxSuccesors(Graph g) {
+    int max = 0;
+    int vMax = 0;
+    int current = 0;
+    ArrayList<Integer> listaS = new ArrayList<Integer>();
+    for(int i = 0 ;i<g.size;i++){
+      listaS = g.getSuccessors(i);
+      current = listaS.size();
+      if(current > max){
+        vMax = i;
+        max = current;
+      }
+      listaS.clear();
+    }
+    return vMax;
+  }
+
+  public static void colombianGraph() throws IOException {
+    BufferedReader scan = new BufferedReader(new FileReader("medellin_colombia-grande.txt"));
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(310153);
+    String cadena = scan.readLine();
+    int i = 0;
+    DigraphAL colombia = new DigraphAL(310153);
+
+    cadena = scan.readLine();
+    while(!cadena.equals(" ")) {
+      String[] info = cadena.split(" ");
+      map.put(Integer.parseInt(info[0]),i);
+      i += 1;
+      cadena = scan.readLine();
+    }
+    
+    cadena = scan.readLine();
+    cadena = scan.readLine();
+    while(!cadena.equals(" ")) {
+      String[] infoArc = cadena.split(" ");
+      colombia.addArc(map.get(Integer.parseInt(infoArc[0])),
+                      map.get(Integer.parseInt(infoArc[1])),
+                      Integer.parseInt(infoArc[2])
+                      );
+    }
+  }
+
+  private static int minVertex (int [] dist, boolean [] v) {
+    int x = Integer.MAX_VALUE;
+    int y = -1;   // graph not connected, or no unvisited vertices
+    for (int i=0; i<dist.length; i++) {
+      if (!v[i] && dist[i]<x) {y=i; x=dist[i];}
+    }
+      return y;
+  }
+  
+    
+  static int [] dijsktra(Graph dg, int source)
+  {
+          final int [] dist = new int [dg.size()];  // shortest known distance from "s"
+         final int [] pred = new int [dg.size()];  // preceeding node in path
+         final boolean [] visited = new boolean [dg.size()]; // all false initially
+   
+         for (int i=0; i<dist.length; i++) {
+           dist[i] = Integer.MAX_VALUE; //Infinity
+       }
+        dist[source] = 0;
+  
+        for (int i=0; i<dist.length; i++) {
+           final int next = minVertex (dist, visited);
+           visited[next] = true;
+  
+           // The shortest path to next is dist[next] and via pred[next].
+  
+           final ArrayList<Integer> n = dg.getSuccessors (next); 
+           for (int j=0; j<n.size(); j++) {
+              final int v = n.get(j);
+              final int d = dist[next] + dg.getWeight(next,v);
+              if (dist[v] > d) {
+                 dist[v] = d;
+                 pred[v] = next;
+              }
+           }
+        }
+        return pred;  // (ignore pred[s]==0!)
+  }
+  
+  
+       public static ArrayList getPath (int [] pred, int s, int e) {
+        final java.util.ArrayList path = new java.util.ArrayList();
+       int x = e;
+        while (x!=s) {
+           path.add (0, x);
+           x = pred[x];
+        }
+        path.add (0, s);
+        return path;
+     }
+ 
+ // Código para dibujar el grafo en GraphViz
+ // Recomiendo www.webgraphviz.com/
+     public static void dibujarGrafo(Graph g)
+     {
+        System.out.println("digraph Grafo {");
+        System.out.println("node [color=cyan, style=filled];");
+        int nv = g.size();
+        for (int i = 0; i < nv; i++)
+        {
+           ArrayList<Integer> lista = g.getSuccessors(i);
+           for (int j = 0; j < lista.size(); j++)
+             System.out.println("\"" + i + "\" -> \"" + lista.get(j) + "\" [ label=\""+ g.getWeight(i,lista.get(j)) +"\"];");
+        }
+        System.out.println("}");
+     }
+ 
+ 
+ public static void main(String[] args) throws IOException
+ {
+  colombianGraph();
+     DigraphAL dgal = new DigraphAL(5);
+     dgal.addArc(0,1,10);
+     dgal.addArc(0,2,3);
+     dgal.addArc(1,2,1);
+     dgal.addArc(1,3,2);
+     dgal.addArc(2,1,4);
+     dgal.addArc(2,3,8);
+     dgal.addArc(2,4,2);
+     dgal.addArc(3,4,7);
+     dgal.addArc(4,3,9);
+
+     System.out.println("max succesor of AL: ");
+     System.out.println(maxSuccesors(dgal));
+     System.out.println("END Max");
+     
+     System.out.println(getPath(dijsktra(dgal,0),0,3));
+     
+     DigraphAM dgam = new DigraphAM(5);
+     dgam.addArc(0,1,10);
+     dgam.addArc(0,2,3);
+     dgam.addArc(1,2,1);
+     dgam.addArc(1,3,2);
+     dgam.addArc(2,1,4);
+     dgam.addArc(2,3,8);
+     dgam.addArc(2,4,2);
+     dgam.addArc(3,4,7);
+     dgam.addArc(4,3,9);
+
+     System.out.println("max succesor of AM: ");
+     System.out.println(maxSuccesors(dgal));
+     System.out.println("END Max");
+     
+     System.out.println(getPath(dijsktra(dgam,0),0,3)); 
+
+     DigraphAlgorithms.dibujarGrafo(dgal);
+     
+ }
+}
--- talleres/taller03/Taller3.java
+++ talleres/taller03/Taller3.java
@@ -3,41 +3,44 @@ import java.util.Collections;
 
 /**
  * Clase en la cual se implementan los metodos del Taller de Clase #3
- * 
- * @author Mauricio Toro, Mateo Agudelo
+ *
+ * @author Rafael Villegas, Felipe Cortes, Mauricio Toro, Mateo Agudelo
  */
 public class Taller3 {
-	
-	private static boolean puedoPonerReina(int r, int c, int[] tablero) {
-    for(int i = 0; i < tablero.length; i++) {
-      if((Math.abs(tablero[r] - i) == Math.abs(r - i))) {
-        return false;
-      }  
-    }
-    return true;
-	}
-	
-	public static int nReinas(int n) {
-    int[] tablero = new int[n];
 
-    return nReinas(0, n, tablero);
+	private static boolean puedoPonerReina(int r, int c, int[] tablero) {
+		for (int i = 0; i < r; i++) {
+			if (tablero[i] == c) {
+				return false;
+			}
+			if (Math.abs(tablero[i] - c) == Math.abs(i - r)) {
+				return false;
+			}
+		}
+		return true;
 	}
-	
+
+	public static int nReinas(int n) {
+		int[] tablero = new int[n];
+
+		return nReinas(0, n, tablero);
+	}
+
 	private static int nReinas(int r, int n, int[] tablero) {
-		if(r>n){
-      imprimirTablero(tablero);
-      return 1;
-    }
-    int s = 0;
-    for(int i=0;i<n;i++){
-      if(puedoPonerReina(r, i, tablero)){
-        tablero[r]=i;
-        s = s + nReinas(r+1, n, tablero);
-      }
-    }
-    return s;
+		if (r == n) {
+			imprimirTablero(tablero);
+			return 1;
+		}
+		int s = 0;
+		for (int i = 0; i < n; i++) {
+			if (puedoPonerReina(r, i, tablero)) {
+				tablero[r] = i;
+				s = s + nReinas(r + 1, n, tablero);
+			}
+		}
+		return s;
 	}
-	
+
 	public static void imprimirTablero(int[] tablero) {
 		int n = tablero.length;
 		System.out.print("    ");
@@ -52,18 +55,72 @@ public class Taller3 {
 		}
 		System.out.println();
 	}
-	
-	//public static ArrayList<Integer> camino(Digraph g, int inicio, int fin) {
-		// complete...
-	//}
 
-	// recomendacion
-	//private static boolean dfs(Digraph g, int nodo, int objetivo, boolean[] visitados, ArrayList<Integer> list) {
-		// complete...
-  //}
-  
-  public static void main(String[] args) {
-    System.out.println(nReinas(4));
-  }
+	public static ArrayList<Integer> camino(DigraphAL g, int inicio, int fin) {
+		ArrayList<Integer> camino = new ArrayList<Integer>();
+		if (dfs(g, inicio, fin, camino)) {	
+			return camino;
+		}
+		camino.clear();
+		return camino;
+	}
+
+	private static boolean dfs(DigraphAL g, int nodo, int objetivo, ArrayList<Integer> camino) {
+		camino.add(nodo);
+		if(nodo == objetivo) {
+			return true;
+		}
+		else if(g.lista.get(nodo).size() == 0) {
+			return false;
+		}
+		for(int i = 0; i < g.lista.get(nodo).size(); i++) {
+			if(!dfs(g, g.lista.get(nodo).get(i).getKey(), objetivo, camino)) {
+				camino.remove(camino.size()-1);
+			}
+			if(camino.get(camino.size() - 1) == objetivo) {
+				break;
+			}
+		}
+		return camino.size() > 1 && camino.get(camino.size() - 1) == objetivo;
+	}
+
+	
+
+	public static void main(String[] args) {
+
+		System.out.println("Punto 1");
+		System.out.println();
+		nReinas(4);
+
+		System.out.println("Punto 2");
+		System.out.println();
+		DigraphAL grafo = new DigraphAL(8);
+		grafo.addArc(2, 3, 1);
+		grafo.addArc(2, 7, 1);
+		grafo.addArc(4, 3, 1);
+		grafo.addArc(1, 7, 1);
+		grafo.addArc(1, 6, 1);
+		grafo.addArc(3, 0, 1);
+		grafo.addArc(3, 5, 1);
+		grafo.addArc(7, 5, 1);
+		grafo.addArc(3, 6, 1);
+
+		ArrayList<Integer> ans = camino(grafo, 2, 6);
+		for(int num : ans) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+		ArrayList<Integer> ans2 = camino(grafo, 1, 0);
+		for(int num : ans2) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+		ArrayList<Integer> ans3 = camino(grafo, 1, 5);
+		for(int num : ans3) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+
+	}
 
 }
