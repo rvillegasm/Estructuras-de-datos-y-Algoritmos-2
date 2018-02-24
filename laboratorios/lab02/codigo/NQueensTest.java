969b4ba1ec46c85f4ce0bf441261906aeabcabdb
fcortesj <fcortesj@eafit.edu.co>
2018-02-24 17:05:53 -0500

    Add files via upload
    JUnit

--- laboratorios/lab02/codigo/NQueensTest.java
+++ laboratorios/lab02/codigo/NQueensTest.java
@@ -1,9 +1,27 @@
-import 
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
- * NQueensTest
+ * The test class NQueensTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
  */
-public class NQueensTest {
-
-  
+public class NQueensTest
+{
+    @Test
+    public void nQueens()
+    {
+        int[] answers = {1,0,0,2,10,4,40,92,352};
+        for(int i=1;i<answers.length;i++){
+            if(answers[i] == NQueens.queens(i)){
+                System.out.println(answers[i]);
+            }
+        }
+    }
 }
+
